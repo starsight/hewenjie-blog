6fd7720f8fc69620a0f981d839b3e65f362c3ecc
starsight <172583172@qq.com>
2026-07-05 21:00:47 +0800

    Update site URL to hewenjie

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://hewenjie-blog.vercel.app',
+  site: 'https://hewenjie.vercel.app',
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
